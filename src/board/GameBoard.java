2c83aa6825f998439cd599ee6a4554782ed7be53
Gab <fedou.gab@gmail.com>
2021-12-14 14:43:31 +0100

    ajout

--- src/board/GameBoard.java
+++ src/board/GameBoard.java
@@ -1,5 +1,155 @@
 package board;
 
-public class GameBoard {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import pawn.Pawn;
+
+public class GameBoard {
+    // Propriétés
+    protected static int NB_ROUND = 12;
+
+    // nombre de pions puis de tentatives
+    int nbSlot;
+    int nbShot = 0;
+
+    // combinaison cachée et combinaison courante user
+    ArrayList<Integer> hiddenCombination;
+    ArrayList<Integer> currentCombination;
+
+    // combinaison essayées
+    int[] tentatives;
+
+    // est ce que le joueur à réussi
+    boolean isResolved = false;
+
+    // statistique de partie
+    // nb victoire / defaite / nb total de parties / nb coup moyen pour la victoire
+    int nbVictoire;
+    int nbDefaite;
+    int nbTotalParties;
+    double nbCoupsMoyen = 0;
+
+    // Constructeur
+    public GameBoard (ArrayList<Integer> combi, int nbSlot) {
+        this.hiddenCombination = combi;
+        this.nbSlot = nbSlot;
+    }
+
+    // getter
+    public static int getNbRound() {
+        return NB_ROUND;
+    }
+    public int getNbSolt() {
+        return this.nbSlot;
+    }
+    public int getNbShot() {
+        return this.nbShot;
+    }
+    public ArrayList<Integer> getHiddenCombination() {
+        return this.hiddenCombination;
+    }
+    public String getHiddenCombinationString() {
+        return affCombiInt(this.hiddenCombination);
+    }
+    public int[] getTentatives() {
+        return this.tentatives;
+    }
+    public boolean getIsResolved () {
+        return this.isResolved;
+    }
+
+    // setter
+    public void setNbSolt(int nb) {
+        this.nbSlot = nb;
+    }
+    public void getNbShot(int nb) {
+        this.nbShot = nb;
+    }
+    public void setHiddenCombination(ArrayList<Integer> combi, boolean isAleatoire) {
+        if (isAleatoire) {
+            this.hiddenCombination = generation_combinaison(nbSlot, true) ;
+        } else {
+            this.hiddenCombination = creation_table_utilisateur(nbSlot);
+        }
+    }
+    public void setCurrentCombination (ArrayList<Integer> combi) {
+        this.currentCombination = combi;
+    }
+    public void setIsResolved (boolean isIt) {
+        this.isResolved = isIt;
+    }
+    
+    // méthodes
+    public String affCombiInt (ArrayList<Integer> tab) {
+        String s = "[";
+        for (int i = 0; i < tab.size() - 1; i ++) {
+            s += String.valueOf(tab.get(i) + ", ");
+        }
+        s += String.valueOf(tab.get(tab.size()-1) + "]"); 
+        return s;
+    }
+
+    public static String affCombiPawn (ArrayList<Pawn> tab) {
+        String s = "[";
+        for (int i = 0; i < tab.size() - 1; i ++) {
+            s += String.valueOf(tab.get(i).couleur) + ", ";
+        }
+        s += String.valueOf(tab.get(tab.size()-1).couleur) + "]"; 
+        return s;
+    }
+
+    public static ArrayList<Integer> generation_combinaison (int nb_pions, boolean combinaison_sans_double) {	
+        ArrayList<Integer> arrCombinaisonAleatoire = new ArrayList<Integer>();
+        if (nb_pions>3 && nb_pions<9) {
+            for (int i = 0; i < nb_pions; i++) {
+                arrCombinaisonAleatoire.add(i, (int)Math.floor((Math.random()*(8-1+1))+1));
+                if (combinaison_sans_double) {
+                    for (int j = 0; j < i; j++) {
+                        if (arrCombinaisonAleatoire.get(j) == arrCombinaisonAleatoire.get(i)) {
+                            arrCombinaisonAleatoire.remove(i);
+                            i-=1;
+                        }
+                    }
+                }  
+            }
+        } else {
+            System.out.println("erreur de saisie");
+        }
+        return arrCombinaisonAleatoire;
+    }
+
+    public static ArrayList<Integer> creation_table_utilisateur(int nb_pions) {
+        ArrayList<Integer> arrCombinaisonUtilisateur = new ArrayList<Integer>();
+        Scanner input = new Scanner(System.in) ;
+        for (int i = 0 ; i < nb_pions; i ++) {
+            arrCombinaisonUtilisateur.add(input.nextInt());
+        }
+        //input.close();
+        return arrCombinaisonUtilisateur;
+    }
+
+    public static int[] comparaison_combinaison (ArrayList<Integer> combi_un, ArrayList<Integer> combi_deux) {
+        int presenceCounter = 0;
+        int positionCounter = 0;
+        int[] arrInfo = new int[2];
+        if (combi_un.size() == combi_deux.size()) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j ++) {
+                    if (combi_un.get(i) == combi_deux.get(j)) {
+                        presenceCounter += 1;
+                        break; // ?
+                    }
+                }
+            }
+            for (int i = 0; i < 4; i++) {
+                if (combi_un.get(i) == combi_deux.get(i)) {
+                    positionCounter += 1;
+                }
+            }
+            arrInfo[0] = positionCounter;
+            arrInfo[1] = presenceCounter - positionCounter;
+        }
+    return arrInfo;
+    }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,106 @@
 package main;
 
+import pawn.Pawn;
+import board.GameBoard;
+import java.util.Scanner;
+
 public class Main {
+    public static void main (String[] args) {
 
-	public static void main(String[] args) {
-		
+        int startStop = 1;
+        int nbPawn = 4;
+        int isUnique = 1;
+        int[] tableResult = new int[2];
 
-	}
+        // Création partie
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Une nouvelle partie s'est crée...");
+        System.out.println("pressez 1 pour jouer, 0 pour quitter");
+        startStop = input.nextInt();
+
+        // jeu
+        
+        while (startStop == 1) {
+
+            // Initialisation game board
+
+            System.out.println(
+                "Veuillez sélectionner le nombre de pions avec lequel vous souhaitez jouer\n"+
+                "Il doit être compris entre 4 et 8 (compris)"
+            );
+            nbPawn = input.nextInt();
+
+            if (nbPawn < 3 || nbPawn > 9) {
+                nbPawn = 4;
+                System.out.println(
+                    "Vous avez fait une erreur en choisissant,\n"+
+                    "le nombre de pions à été mis à 4 par défaut"
+                );
+            }
+
+            GameBoard gameBoardOne = new GameBoard(GameBoard.generation_combinaison(nbPawn, true), nbPawn);
+            System.out.println(
+                "Veuillez maintenant sélectionner si les pions de la combinaisons cachée doivent être unique\n"+
+                "(1) sinon (0)");
+            isUnique = input.nextInt();
+
+            if (isUnique == 0) {
+                gameBoardOne = new GameBoard(GameBoard.generation_combinaison(nbPawn, false), nbPawn);
+            } else if (isUnique == 1) {
+            } else {
+                System.out.println("Erreur de saisie, les pions seront uniques par défault");
+            }
+            System.out.println(gameBoardOne.getHiddenCombinationString());
+            while (gameBoardOne.isResolved == false && gameBoardOne.nbShot < 12) {
+                gameBoardOne.nbShot+=1;//set
+                System.out.println("il vous reste : " + (13 - gameBoardOne.nbShot) + " éssais...");
+                System.out.println("Entrez votre selection de pions :");
+                gameBoardOne.setCurrentCombination(gameBoardOne.creation_table_utilisateur(nbPawn));
+                tableResult = gameBoardOne.comparaison_combinaison(gameBoardOne.hiddenCombination, gameBoardOne.currentCombination);
+                if (tableResult[0] == 4) {
+                    gameBoardOne.isResolved = true;//set
+                } else {
+                    System.out.println(
+                        "Vous avez " + 
+                        tableResult[0] + " pions bien positionnés et de bonne couleur " + 
+                        tableResult[1] + " pions de bonne couleur."
+                    );
+                }
+            }
+
+            if (gameBoardOne.isResolved == false) {
+                System.out.println(
+                    "vous venez de perdre la partie, la combinaison cachée était :" +
+                    gameBoardOne.affCombiInt(gameBoardOne.hiddenCombination)
+                );
+            } else {
+                System.out.println(
+                    "Vous venez de trouver la combinaison cachée : " +
+                    gameBoardOne.affCombiInt(gameBoardOne.hiddenCombination)
+                );
+                gameBoardOne.nbVictoire +=1;//set
+                gameBoardOne.nbCoupsMoyen = (gameBoardOne.nbShot + gameBoardOne.nbCoupsMoyen * (gameBoardOne.nbVictoire - 1)) / gameBoardOne.nbVictoire;//set get
+                gameBoardOne.isResolved = false;
+            }
+            gameBoardOne.nbShot = 0; //set
+            gameBoardOne.nbTotalParties+=1; //set
+            gameBoardOne.nbDefaite=gameBoardOne.nbTotalParties-gameBoardOne.nbDefaite; //set/get
+
+            System.out.println("pressez 1 pour rejouer, 0 pour quitter");
+            startStop= input.nextInt();
+
+            if (startStop == 0) {
+                System.out.println(
+                    "Merci d'avoir joué, votre résultat est le suivant :\n" +
+                    gameBoardOne.nbVictoire + " parties gagnées " +
+                    " sur " + gameBoardOne.nbTotalParties +
+                    " parties jouées.\n" + 
+                    "Et en moyenne " + gameBoardOne.nbCoupsMoyen +
+                    " coups pour résoudre une partie"
+                );
+            }
+        }
+        input.close();
+    }
 }
